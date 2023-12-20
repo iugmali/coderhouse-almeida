0a6b7c91c344d4f45f598bfc88013c44f5954f99
Guilherme Pinto Lima de Almeida <dev@iugmali.com>
2023-12-20 01:46:40 -0300

    feat(auth): retrieve user info on layout
    retrieve auth user info on layout and pass as props to navbar component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,27 +2,26 @@ import type { Metadata } from 'next'
 import './globals.css'
 import React from "react";
 import Navbar from "@/components/Navbar";
-import {CartContextProvider} from "@/context/cartContext";
 import {inter} from "@/app/fonts";
-
+import {logout} from "@/lib/actions/auth";
+import {auth} from "@/lib/auth";
 
 export const metadata: Metadata = {
   title: 'Coderstore Almeida',
-  description: 'Criado para o curso da Coderhouse',
+  description: 'Loja virtual onde se encontra de tudo um pouco.',
 }
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const session = await auth();
   return (
     <html lang="pt-BR">
       <body className={`min-h-screen bg-gray-50 ${inter.className} antialiased`}>
-        <CartContextProvider>
-          <Navbar />
+          <Navbar logout={logout} session={session}/>
           {children}
-        </CartContextProvider>
       </body>
     </html>
   )
