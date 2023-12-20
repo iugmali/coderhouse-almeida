6eac33b47dbf25095282096640832a6f5cafdeec
Guilherme Pinto Lima de Almeida <dev@iugmali.com>
2023-12-20 01:48:20 -0300

    style(navbar): change navbar style
    change navbar main styles

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,73 +1,119 @@
 'use client';
 
 import Link from "next/link";
-import {useContext} from "react";
 import CartWidget from "@/components/ui/CartWidget";
 import {useParams} from "next/navigation";
-import CartContext from "@/context/cartContext";
 import {jetBrainsMono} from "@/app/fonts";
-import {UserIcon} from "@heroicons/react/24/outline";
+import {UserIcon as UserIconOutLine} from "@heroicons/react/24/outline";
+import {UserIcon as UserIconSolid} from "@heroicons/react/24/solid";
+import {useFormStatus} from "react-dom";
+import Spinner from "@/components/ui/Spinner";
+import {Session} from "next-auth";
 
-const Navbar = () => {
-  const {totalItems} = useContext(CartContext);
+type Props = {
+  logout: () => Promise<void>
+  session: Session | null
+}
+
+const Navbar = ({logout, session}:Props) => {
   const {category} = useParams();
+
   return (
       <nav
         className="sticky top-0 z-50 bg-gradient-to-b from-gray-800 to-gray-900 drop-shadow-lg text-gray-50"
       >
         <div className="flex px-8 py-6 justify-between items-center mx-auto max-w-3xl">
-          <Link className={`${jetBrainsMono.className} font-extrabold p-3`} href={'/'} scroll={false}>Coderstore</Link>
+          <div className={`relative`}>
+            <div className={`group rounded-3xl shadow-sm shadow-amber-50 transition-all hover:shadow-lg hover:shadow-amber-50 px-2`}>
+              <Link className={`${jetBrainsMono.className} font-extrabold text-xl p-3`} href={'/'} scroll={false}>Coderstore</Link>
+            </div>
+          </div>
           <ul className={`hidden p-3 md:flex md:flex-row gap-8`}>
             <li
-              className={`${(category === 'artigos') ? 'text-blue-400' : ''} hover:opacity-50 active:text-blue-400`}>
-              <Link href={'/artigos'} scroll={false}> Artigos</Link>
+              className={`${jetBrainsMono.className} ${(category === 'linguagens') ? 'text-blue-400' : ''} flex-1 font-medium text-sm hover:opacity-50 active:text-blue-400`}>
+              <Link href={'/linguagens'} scroll={false}>Linguagens</Link>
             </li>
-            <li className={`${(category === 'livros') ? 'text-blue-400' : ''} hover:opacity-50 active:text-blue-400`}>
-              <Link href={'/livros'} scroll={false}>Livros</Link></li>
-            <li className={`${(category === 'videos') ? 'text-blue-400' : ''} hover:opacity-50 active:text-blue-400`}>
-              <Link href={'/videos'} scroll={false}>Vídeos</Link></li>
+            <li
+              className={`${jetBrainsMono.className} ${(category === 'bibliotecas') ? 'text-blue-400' : ''} flex-1 font-medium text-sm hover:opacity-50 active:text-blue-400`}>
+              <Link href={'/bibliotecas'} scroll={false}>Bibliotecas</Link>
+            </li>
+            <li
+              className={`${jetBrainsMono.className} ${(category === 'frameworks') ? 'text-blue-400' : ''} flex-1 font-medium text-sm hover:opacity-50 active:text-blue-400`}>
+              <Link href={'/frameworks'} scroll={false}>Frameworks</Link>
+            </li>
           </ul>
           <div className={`flex justify-end`}>
             <button className={`group p-3 mx-2 md:hidden`}>
-              <div className={`transition-all space-y-2 group-focus-within:space-y-0`}>
-                <span className={`block w-6 h-0.5 bg-gray-300 transition-all group-focus-within:rotate-45`}></span>
+              <div className={`transition-all space-y-2 group-focus-within:space-y-0 group-focus-within:space-x-3`}>
+                <span className={`block w-6 h-0.5 bg-gray-300 transition-all group-focus-within:rotate-[60deg]`}></span>
                 <span
                   className={`block w-6 h-0.5 bg-gray-300 transition-all group-focus-within:translate-x-100 group-focus-within:hidden`}></span>
                 <span
-                  className={`block w-6 h-0.5 bg-gray-300 transition-all group-focus-within:-rotate-45 group-focus-within:-translate-y-0.5`}></span>
+                  className={`block w-6 h-0.5 bg-gray-300 transition-all group-focus-within:-rotate-[60deg] group-focus-within:-translate-y-0.5`}></span>
               </div>
               <div
-                className={`z-50 hidden group-focus-within:block md:group-focus-within:hidden absolute right-24 top-[100px] w-40 px-8 py-4 bg-gradient-to-b from-gray-900 to-gray-800 shadow-lg text-gray-50 rounded-2xl`}>
+                className={`z-50 hidden group-focus-within:block md:group-focus-within:hidden absolute right-[74px] top-[100px] w-40 px-8 py-4 bg-gradient-to-b from-gray-900 to-gray-800 shadow-lg text-gray-50 rounded-2xl`}>
                 <ul className={`flex flex-col items-center gap-8`}>
                   <li
-                    className={`${(category === 'artigos') ? 'text-blue-400' : ''} hover:opacity-50 active:text-blue-400`}>
-                    <Link href={'/artigos'} scroll={false}> Artigos</Link>
+                    className={`${(category === 'linguagens') ? 'text-blue-400' : ''} hover:opacity-50 active:text-blue-400`}>
+                    <Link href={'/linguagens'} scroll={false}>Linguagens</Link>
                   </li>
                   <li
-                    className={`${(category === 'livros') ? 'text-blue-400' : ''} hover:opacity-50 active:text-blue-400`}>
-                    <Link href={'/livros'} scroll={false}>Livros</Link></li>
+                    className={`${(category === 'bibliotecas') ? 'text-blue-400' : ''} hover:opacity-50 active:text-blue-400`}>
+                    <Link href={'/bibliotecas'} scroll={false}>Bibliotecas</Link>
+                  </li>
                   <li
-                    className={`${(category === 'videos') ? 'text-blue-400' : ''} hover:opacity-50 active:text-blue-400`}>
-                    <Link href={'/videos'} scroll={false}>Vídeos</Link></li>
-                </ul>
-              </div>
-            </button>
-            <button className={`group p-3 mx-1 relative`}>
-              <UserIcon className={`w-6 h-6`}/>
-              <div
-                className={`z-50 hidden group-focus-within:block absolute -right-10 top-[76px] w-40 px-8 py-4 bg-gradient-to-b from-gray-900 to-gray-800 shadow-lg text-gray-50 rounded-2xl`}>
-                <ul className={`flex flex-col items-center gap-8`}>
-                  <li
-                    className={`hover:opacity-50 active:text-blue-400`}>
-                    <Link href={'#'} scroll={false}>Entrar</Link>
+                    className={`${(category === 'frameworks') ? 'text-blue-400' : ''} hover:opacity-50 active:text-blue-400`}>
+                    <Link href={'/frameworks'} scroll={false}>Frameworks</Link>
                   </li>
                 </ul>
               </div>
             </button>
-            <Link className={`mt-1 p-3`} href={`/cart`} scroll={false}><CartWidget quantity={totalItems}/></Link>
+            <div className={`group p-3 mx-1 relative hover:cursor-pointer`} tabIndex={0}>
+              {(session && session.user) ? (
+                <UserIconSolid className={`absolute w-6 h-6 top-4 left-0 hover:opacity-50`}/>
+              ) : (
+                <UserIconOutLine className={`absolute w-6 h-6 top-4 left-0 hover:opacity-50`}/>
+              )}
+              <div
+                className={`z-50 hidden group-focus-within:block absolute -right-[68px] top-[76px] w-40 px-8 py-4 bg-gradient-to-b from-gray-900 to-gray-800 shadow-lg text-gray-50 rounded-2xl`}>
+                <ul className={`flex flex-col items-center gap-8`}>
+                  {(session && session.user) ? (
+                      <>
+                        <li
+                          className={`hover:opacity-50 active:text-blue-400`}>
+                          <Link href={'/profile'} scroll={false}>Meu perfil</Link>
+                        </li>
+                        <li
+                          className={`hover:opacity-50 active:text-blue-400`}>
+                          <form action={logout}>
+                            <LogoutButton />
+                          </form>
+                        </li>
+                      </>
+                    )
+                    :
+                    <li
+                      className={`hover:opacity-50 active:text-blue-400`}>
+                      <Link href={'/login'} scroll={false}>Entrar</Link>
+                    </li>
+                  }
+                </ul>
+              </div>
+            </div>
+            <Link className={`mt-1 p-3`} href={`/cart`} scroll={false}><CartWidget /></Link>
           </div>
         </div>
       </nav>
+  );
+}
+
+const LogoutButton = () => {
+  const { pending } = useFormStatus();
+  return (
+    <button aria-disabled={pending} disabled={pending}>
+      {pending ? <span className={`flex justify-center`}><p>Saindo...</p><Spinner className={'h-5 w-5 border border-r-white border-b-white'}/></span> : <span>Sair</span>}
+    </button>
   );
 }
 
