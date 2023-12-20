fa4f95f4a5358bbb186558e2147df20baee776c4
Guilherme Pinto Lima de Almeida <dev@iugmali.com>
2023-12-20 01:44:28 -0300

    refactor(cart): refactor cart components
    refactor cart components

--- src/components/cart/CartItem/index.tsx
+++ src/components/cart/CartItem/index.tsx
@@ -1,54 +1,68 @@
-import Button from "@/components/ui/Button";
+'use client'
+
+import Button, {TextButton} from "@/components/ui/Button";
 import {motion} from "framer-motion";
-import {CartItem} from "@/types/cart";
+import {TCartItem} from "@/types/cart";
 import {useRouter} from "next/navigation";
-import {useContext} from "react";
-import CartContext from "@/context/cartContext";
 import {formatCurrency} from "@/lib/util";
 import {jetBrainsMono, montserrat} from "@/app/fonts";
+import {MinusIcon, PlusIcon} from "@heroicons/react/20/solid";
+import {TrashIcon} from "@heroicons/react/24/outline";
+import {useCartStore} from "@/store/cart.store";
 
 type Props = {
-  cartItem: CartItem
+  cartItem: TCartItem;
 }
 
 const CartItem = ({cartItem}: Props) => {
-  const {onRemoveItem, onAddItem} = useContext(CartContext);
+  const onSubtractItem = useCartStore(state => state.onSubtractItem);
+  const onAddItem = useCartStore(state => state.onAddItem);
+  const onRemoveItem = useCartStore(state => state.onRemoveItem);
+
   const router = useRouter();
 
   return (
-    <motion.li
-      layout
-      key={cartItem.id}
-      exit={{
-        y: -30,
-        opacity: 0,
-        transition: {duration: 0.3}
-      }}
-      className={`rounded-lg px-4 py-2 border flex items-center justify-between border-gray-600 hover:border-gray-950`}
-    >
-      <div className={`flex-[3] flex flex-col md:flex-row hover:cursor-pointer`}
-           onClick={() => router.push(`/item/${cartItem.id}`, {scroll: false})}>
-        <p className={`${montserrat.className} flex-1`}>{cartItem.title}</p>
-        <p className={`flex-1`}>Quantidade: <span className={`${jetBrainsMono.className}`}>{cartItem.quantity}</span></p>
-        <p className={`flex-1`}>Total: <span className={`${jetBrainsMono.className}`}>{formatCurrency(cartItem.quantity * cartItem.price)}</span></p>
-      </div>
-      <div className={`flex-1 flex flex-col-reverse md:flex-row gap-4 items-end md:justify-end`}>
-        <Button
+      <motion.li
+        layout
+        key={cartItem.id}
+        exit={{
+          y: -30,
+          opacity: 0,
+          transition: {duration: 0.3}
+        }}
+        className={`rounded-lg px-4 py-2 border flex items-center justify-between border-gray-600 hover:border-gray-950`}
+      >
+        <div className={`flex-1 flex flex-col md:flex-row md:justify-between md:items-center hover:cursor-pointer`}
+             onClick={() => router.push(`/item/${cartItem.id}`, {scroll: false})}>
+          <p className={`px-1 ${montserrat.className} flex-[2]`}>{cartItem.title}</p>
+          <p className={`px-1 flex-1`}>Quantidade: <span
+            className={`${jetBrainsMono.className}`}>{cartItem.quantity}</span></p>
+          <p className={`px-1 flex-1`}>Total: <span
+            className={`${jetBrainsMono.className}`}>{formatCurrency(cartItem.quantity * cartItem.price)}</span></p>
+        </div>
+        <div className={`flex flex-col-reverse md:flex-row gap-4 items-end md:justify-end`}>
+          <TextButton
+            title={`subtrair`}
+            className={`hover:bg-gray-200`}
+            handleClick={() => {
+              onSubtractItem(cartItem.id);
+            }}>
+            <MinusIcon className={`h-3 w-3`}/>
+          </TextButton>
+          <TextButton
+            title={`adicionar`}
+            className={`hover:bg-gray-200`}
+            handleClick={() => onAddItem({...cartItem, quantity: 1})}>
+            <PlusIcon className={`h-3 w-3`}/>
+          </TextButton>
+        </div>
+        <TextButton
           title={`remover`}
-          className={`bg-gray-50 text-gray-950 transition-colors hover:bg-gray-200`}
-          handleClick={() => {
-            onRemoveItem(cartItem.id);
-          }}>
-          -
-        </Button>
-        <Button
-          title={`adicionar`}
-          className={`bg-gray-50 text-gray-950 transition-colors hover:bg-gray-200`}
-          handleClick={() => onAddItem({...cartItem, quantity: 1})}>
-          +
-        </Button>
-      </div>
-    </motion.li>
+          className={`hover:bg-gray-200`}
+          handleClick={() => onRemoveItem(cartItem.id)}>
+          <TrashIcon className={`h-6 w-6 text-red-800`}/>
+        </TextButton>
+      </motion.li>
   )
 }
 
---
+++ src/components/cart/Checkout/index.tsx
@@ -0,0 +1,96 @@
+'use client'
+
+import {useEffect, useState} from "react";
+import {useCartStore} from "@/store/cart.store";
+import {jetBrainsMono, montserrat} from "@/app/fonts";
+import {formatCurrency} from "@/lib/util";
+import Button, {LinkButton} from "@/components/ui/Button";
+import {useFormState, useFormStatus} from "react-dom";
+import Spinner from "@/components/ui/Spinner";
+import {AnimatePresence} from "framer-motion";
+import Modal from "@/components/ui/Modal";
+import {useRouter} from "next/navigation";
+
+const Checkout = ({user, placeOrder}: any) => { // precisa ser any enquanto nao resolvem o bug das server actions com firebase-admin, que só podem ser passados por props não tipadas
+  const router = useRouter();
+  const cartItems = useCartStore(state => state.cartItems)
+  const totalPrice = useCartStore(state => state.totalPrice)
+  const clearCart = useCartStore(state => state.clearCart);
+
+  const initialState = {message: null};
+  const [state, dispatch] = useFormState(placeOrder.bind(null, cartItems, user.email, totalPrice), initialState)
+
+  const handleConfirmOrder = () => {
+    clearCart();
+    router.push('/profile')
+  }
+
+  // Esse código precisa ser passado para clients components que utilizam zustand com persist, para garantir que o zustand rode depois do nextjs hidratar
+  const [hasHydrated, setHasHydrated] = useState(false);
+  useEffect(() => {
+    useCartStore.persist.rehydrate();
+    setHasHydrated(true);
+  }, []);
+  if (!hasHydrated) return null;
+
+  return (
+      (cartItems.length > 0) ? (
+        <>
+          <h1 className={`${montserrat.className} text-center text-2xl`}>Resumo do seu Pedido</h1>
+          <AnimatePresence>
+            {state && state.message && (
+              <Modal onClose={() => {}}>
+                <h2 className={`${montserrat.className} text-xl text-center m-4`}>Pedido Confirmado</h2>
+                <p className={`${jetBrainsMono.className} text-center`}>ID: {state.message}</p>
+                <div className={`w-full flex mt-4 mb-4 justify-center`}>
+                  <Button className={`bg-green-800`} handleClick={handleConfirmOrder}>Ir para o meu perfil</Button>
+                </div>
+              </Modal>
+            )}
+          </AnimatePresence>
+
+          <ul className={`px-14 flex flex-col gap-4 mt-4`}>
+            {cartItems.map(cartItem => (
+              <li
+                key={cartItem.id}
+                className={`rounded-lg px-4 py-2 border flex items-center justify-between border-gray-600 hover:border-gray-950`}
+              >
+                <div
+                  className={`flex-1 flex flex-col md:flex-row md:justify-between md:items-center hover:cursor-pointer`}>
+                  <p className={`px-1 ${montserrat.className} flex-[2]`}>{cartItem.title}</p>
+                  <p className={`px-1 flex-1`}>Quantidade: <span
+                    className={`${jetBrainsMono.className}`}>{cartItem.quantity}</span></p>
+                  <p className={`px-1 flex-1`}>Total: <span
+                    className={`${jetBrainsMono.className}`}>{formatCurrency(cartItem.quantity * cartItem.price)}</span>
+                  </p>
+                </div>
+              </li>
+            ))}
+            <li className={`flex w-3/4 max-w-3xl mx-auto py-2 justify-between items-center`}>
+              <p className={`text-xl font-bold`}>Total</p>
+              <p className={`${jetBrainsMono.className} text-xl font-bold`}>{formatCurrency(totalPrice)}</p>
+            </li>
+          </ul>
+          <form action={dispatch}>
+            <PlaceOrderButton />
+          </form>
+        </>
+      ) : (
+        <>
+          <p className={`text-center`}>Não existem itens no seu carrinho</p>
+          <LinkButton className={``} href={`/`}>Retornar para a tela inicial</LinkButton>
+        </>
+      )
+  );
+}
+
+const PlaceOrderButton = () => {
+  const { pending } = useFormStatus();
+  return (
+    <Button className="w-60 mx-auto rounded-3xl py-4 px-4 mt-4 flex justify-evenly items-center bg-green-800" aria-disabled={pending} disabled={pending}>
+      {pending ? <><p>Confirmando...</p><Spinner className={'h-5 w-5 border border-r-white border-b-white'}/></> : <span>Confirmar Pedido</span>}
+    </Button>
+  );
+}
+
+export default Checkout;
